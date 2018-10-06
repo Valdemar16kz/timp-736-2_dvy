a01f53b9fed2279a1e6cc74ff05bad050ab5301f
Valdemar16kz <vovadomoshenko@gmail.com>
2018-10-06 10:27:13 +0400

    commit

---
+++ 736-2_dvy_2-1.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+	int n,i,x,max;
+	scanf("%d",&n);
+        scanf("%d",&x);
+	max=x;
+	for(i=1; i<=n-1; i++)
+		{ scanf("%d",&x);
+		  if (max<=x)
+		    {
+                      max = x;
+                    };
+		}
+	printf("%d\n",max);
+	return 0;
+}	 
